e7ffaefc1bb22b59af9315dece831f7ec511e3c3
RAGAVANM1 <116934731+RAGAVANM1@users.noreply.github.com>
2023-10-14 23:18:20 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -114,8 +114,8 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
             reply_markup=InlineKeyboardMarkup(
                [[InlineKeyboardButton("Original Link", url=share_link),
                   InlineKeyboardButton("Short Link", url=short_link)],
-                 [InlineKeyboardButton("Bots Channel", url="https://t.me/TechyTel"),
-                  InlineKeyboardButton("Support Group", url="https://t.me/TechyTelSupport")]]
+                 [InlineKeyboardButton("Channel", url="https://t.me/TamilMvWorld"),
+                  InlineKeyboardButton("Owner", url="https://t.me/J_S_X_20")]]
             ),
             disable_web_page_preview=True
         )
